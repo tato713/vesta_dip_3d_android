d290c4fa8753a40091b8df8ff6dab9500dceb15f
tato713 <tato_713@hotmail.com>
2020-10-22 23:37:04 -0300

    git

--- src/app/services/modelo.service.spec.ts
+++ src/app/services/modelo.service.spec.ts
@@ -1,25 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ModeloService } from '/modelo.service';
+import { ModeloService } from './modelo.service';
 
 describe('ModeloService', () => {
+  let service: ModeloService;
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ModeloService);
+  });
 
-  public carrito: Array<Modelo> = [];
-  constructor(private httpClien:HttpClient) { }
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-  public obtenerTodos() {
-    return this.httpClien.get<Modelo[]>("http://192.168.0.107:3000/modelos");
-  }
-  public obtenerPorId(id: string) {
-     return this.httpClien.get<Modelo>("http://192.168.0.107:3000/modelos/"+id);
-  }
-
-  public agregar(prod: Modelo) {
-   // this.Modelos.push(prod);
-  
-  }
-  public getCarrito() {
-    return this.carrito;
-  }
 });
