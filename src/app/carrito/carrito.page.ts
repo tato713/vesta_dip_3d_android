923fe4e7dcd3777fa206abb1f45e50d288ba260f
lutzya <lu_cs_16@hotmail.com>
2020-10-27 21:20:26 -0300

    cambio carrito

--- src/app/carrito/carrito.page.ts
+++ src/app/carrito/carrito.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Modelo } from '../model/modelo';
 import { ModeloService } from '../services/modelo.service';
 import { AlertController } from '@ionic/angular';
+// import { EmailComposer } from '@ionic-native/email-composer/ngx';
+// import { from } from 'rxjs';
 
 @Component({
   selector: 'app-carrito',
@@ -10,49 +12,79 @@ import { AlertController } from '@ionic/angular';
 })
 export class CarritoPage implements OnInit {
   private carrito: Array<Modelo> = [];
+  private materiales;
 
-  constructor(private modSrv: ModeloService, public alertController: AlertController) { }
+  constructor(private modSrv: ModeloService, public alertController: AlertController
+    // , private emailComposer: EmailComposer
+  ) { }
 
   public async ngOnInit() {
 
     this.carrito = this.modSrv.carrito;
+    this.materiales = this.modSrv.materiales;
+
+
     
-
-    // const loading = await this.loading.create();   
-    // loading.present();
-
-    // this.modSrv.obtenerTodos().subscribe(datos => {
-
-    //   this.modelos = datos
-    //   loading.dismiss();
-    // });
   }
+
   async presentAlert() {
-   
-          const alert = await this.alertController.create({
-        //cssClass: 'my-custom-class',
-        header: '!',
-        subHeader: 'Los correos no coinciden',
-        message: 'por favor reingrese los datos',
-        buttons: ['OK!']
-      });
-      await alert.present();
-    }
-  public getPrecio(mod:Modelo): number {
+
+    const alert = await this.alertController.create({
+      //cssClass: 'my-custom-class',
+      header: '!',
+      subHeader: 'Los correos no coinciden',
+      message: 'por favor reingrese los datos',
+      buttons: ['OK!']
+    });
+    await alert.present();
+  }
+  public getPrecio(mod: Modelo): number {
 
     return Number(mod.precio)
-      * Number(mod.cantidad) 
+      * Number(mod.cantidad)
       * Number(this.modSrv.materiales[mod.material])
       ;
   }
-public confirmarpedido(maila:string, mailb:string ) {
-if (maila==mailb) {
-  //agregar mail y pedidos al servidor (o a un lugar donde la empresa pueda contactarse y reconocer los datos)
-}
-else {
-  this.presentAlert();
-}
+  // public enviarMail(correo: string) {
+  //   // this.emailComposer.isAvailable().then((available: boolean) =>{
+  //   //   if(available) {
+  //   //     //Now we know we can send
+  //   //   }
+  //   //  });
+     
+  //   let email = {
+  //      from: correo,
+  //      to: 'vestadip3d@gmail.com',
+  //     //  cc: 'erika@mustermann.de',
+  //     //  bcc: ['john@doe.com', 'jane@doe.com'],
+  //     //  attachments: [
+  //     //    'file://img/logo.png',
+  //     //    'res://icon.png',
+  //     //    'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+  //     //    'file://README.pdf'
+  //     //  ],
+  //      subject: 'Confirmar pedido',
+  //      body: 'Bonjourrrr', // acá va detalle del pedido
+  //      isHtml: true
+       
+  //    };
+     
+  //    // Send a text message using default options
+  //    this.emailComposer.open(email);
 
-}
+  // }
+
+
+  public confirmarPedido(maila: string, mailb: string) {
+    if (maila == mailb) {
+      // this.enviarMail(maila);
+
+      //agregar mail y pedidos al servidor (o a un lugar donde la empresa pueda contactarse y reconocer los datos)
+    }
+    else {
+      this.presentAlert();
+    }
+
+  }
 
 }
